40769269fd0ed7071be62acc0d30816c55a5eba2
voximity <zander@zanderf.net>
2020-12-29 15:38:37 -0600

    Rewrite internals, introduce protocol specification

--- README.md
+++ README.md
@@ -45,3 +45,25 @@ If the server is the host server, then the chat server will start when the plugi
 If the server is a client server, then it will attempt to establish a connection with the hostname and port you set in the config. Ensure this happens after the host server is up and running.
 
 To reconnect at any time (during a connection or after losing one), run the `!chat:reconnect` command as host.
+
+## Specification
+
+The host server is responsible for initiating a TCP server. Client instances are responsible for establishing a connection and sending/receiving information after the host server acknowledges its handshake.
+
+A packet is a JSON object that always includes the field `type`, representing the type of packet. Each packet type has expected information to follow. See below for a table of packets and their types.
+
+### Packet types
+
+| **Comes from** | **Packet type** | **Extra fields** | **Description** |
+| --- | --- | --- | --- |
+| client | `handshake` | `name`, `color`, `prefix`, `playerCount` | The handshake is soon as the client makes connection with the host server. The client is expected to send some information about their server instance. `playerCount` is only used once at connection, so it does not need to be updated. |
+| host | `acknowledge` | `identifier`, `hostIdentifier` `connections.identifier`, `connections.name`, `connections.color`, `connections.prefix` | The acknowledge packet is sent in response to the `handshake` packet. If the client does not receive an `acknowledge` packet in response, the connection was unsuccessful. Included is `identifer`, representing the identifier used to distinguish the connecting client from other clients. Clients should store this value as well as `hostIdentifier`, the identifier of the host server, as well as all connections passed in the `connections` array. Clients are expected to maintain an up-to-date directory of connections. The host server will send updates about individual connections as they change. |
+| host | `connection` | `identifier`, `name`, `color`, `prefix`, `playerCount` | The host sends this packet when a new client connects to the host server. The client should store this information in its internal connections array, except for the `playerCount` field, which is only used for immediately displaying the connection to other servers. |
+| host | `disconnect` | `identifier` | The host sends this packet when a client disconnects from the host. The included identifier is the disconnecting client. The client *should* have this client already stored in its internal connections array. Remove it if it exists. |
+| both | `join` | `identifier`, `username` | Both instance types (clients and hosts) can send this packet. It is sent when a player joins one of the connected servers. The `identifier` is the identifier of the instance sending the packet. If the sending instance is a client, then the packet will be relayed to all other clients after being processed by the host server. |
+| both | `leave` | `identifier`, `username` | Similar to `join`, this is sent when a player leaves a server. See above for more information. |
+| both | `message` | `identifer`, `username`, `content` | Similar to `join` and `leave`, this is sent when a user sends a message on a server. The field `content` is expected to be sanitized by the instance sending the packet, NOT the receiving instances. |
+
+### Connections and disconnects
+
+When a client connects/disconnects from the host server, the host server will distribute `connection` and `disconnect` packets respectively. The client does not need to inform the host server of a disconnect, a close of the connection will work.
--- omegga.plugin.js
+++ omegga.plugin.js
@@ -1,4 +1,239 @@
 const net = require("net");
+const {chat: {sanitize, parseLinks}} = OMEGGA_UTIL;
+
+const TEXT_COLOR = (color) => `<color="${color}">`;
+
+// An abstract class representing both the host server and client server
+class ConnectionInstance {
+    constructor(name, color, prefix) {
+        this.name = name;
+        this.color = color;
+        this.prefix = prefix;
+        this.connections = [];
+    }
+
+    getConnection(identifier) {
+        return this.connections.find((c) => c.identifier == identifier);
+    }
+
+    removeConnection(identifier) {
+        this.connections.splice(this.connections.indexOf(this.getConnection(identifier)), 1);
+    }
+
+    start(playerCount) {}
+
+    handlePacket(packet, isHost) {
+        if (packet.type == "connection") {
+            // New connection to the host server
+            const newConnection = isHost ? this.getConnection(packet.identifier) : {identifier: packet.identifier, name: packet.name, color: packet.color, prefix: packet.prefix};
+            if (!isHost) this.connections.push(newConnection);
+
+            // Display in chat
+            const {color, name} = newConnection;
+            this.omegga.broadcast(`${TEXT_COLOR(color)}<b>${newConnection.name}</> has connected to chat with ${packet.playerCount} players online.</>`);
+        } else if (packet.type == "disconnect") {
+            // A connection was closed with the host server
+            const {name, color} = this.getConnection(packet.identifier);
+            this.omegga.broadcast(`${TEXT_COLOR(color)}<b>${name}</> has disconnected from chat.</>`);
+            this.removeConnection(packet.identifier);
+        } else if (packet.type == "join") {
+            // A player joined another connection on the host server
+            const {name, color, prefix} = this.getConnection(packet.identifier);
+            this.omegga.broadcast(`${TEXT_COLOR(color)}<b>${prefix} ${packet.username}</> has joined <b>${name}</>.</>`);
+        } else if (packet.type == "leave") {
+            // A player left another connection on the host server
+            const {name, color, prefix} = this.getConnection(packet.identifier);
+            this.omegga.broadcast(`${TEXT_COLOR(color)}<b>${prefix} ${packet.username}</> has left <b>${name}</>.</>`);
+        } else if (packet.type == "message") {
+            // A player sent a message on another connection on the host server
+            const {color, prefix} = this.getConnection(packet.identifier);
+            this.omegga.broadcast(`${TEXT_COLOR(color)}${prefix} <b>${packet.username}</>:</> ${packet.content}`);
+        }
+    }
+
+    sendJoinPacket(username) {}
+    sendLeavePacket(username) {}
+    sendMessagePacket(username, content) {}
+
+    disconnect(shouldReconnect) {}
+}
+
+// The host server derivation of the above class
+class HostServerInstance extends ConnectionInstance {
+    constructor(name, color, prefix, port) {
+        super(name, color, prefix);
+        this.port = port;
+        this.server = net.createServer(c => this.handleConnection(c));
+        this.currentIdentifier = 0;
+        this.identifier = this.currentIdentifier++;
+    }
+
+    start(playerCount) {
+        this.server.listen(this.port);
+    }
+
+    sendPacketToAll(packet, blacklist) {
+        this.connections.filter((c) => !(blacklist || []).includes(c.identifer)).forEach((c) => c.connection.write(JSON.stringify(packet)));
+    }
+
+    handleConnection(connection) {
+        // New connection. Develop its connection object.
+        const context = {connection, identifier: this.currentIdentifier++, name: null, prefix: null, color: null, receivedHandshake: false};
+        this.connections.push(context);
+
+        connection.on("data", (data) => {
+            const packet = JSON.parse(data);
+            if (!connection.receivedHandshake && packet.type != "handshake") {
+                console.log(`ERROR: connection ${context.identifier} sent non-handshake packet before sending handshake`);
+                connection.destroy();
+                return;
+            }
+
+            if (packet.type == "handshake") {
+                console.log(`INFO: received handshake from connection ID ${context.id}`);
+
+                context.receivedHandshake = true;
+
+                // Create the ack packet
+                const responsePacket = {type: "acknowledge", identifier: context.identifier, hostIdentifier: this.identifier, connections: [...this.connections.map((c) => ({identifier: c.identifier, name: c.name, color: c.color, prefix: c.prefix})), {identifier: this.identifier, name: this.name, color: this.color, prefix: this.prefix}]};
+
+                // Write it to the connection
+                connection.write(JSON.stringify(responsePacket));
+
+                // Create the connection packet for other connections
+                const connectionPacket = {type: "connection", identifier: context.identifier, name: context.name, color: context.color, prefix: context.prefix, playerCount: packet.playerCount};
+
+                // Handle it ourselves
+                this.handlePacket(connectionPacket, true);
+
+                // Distribute to other connections (not the new one)
+                this.sendPacketToAll(connectionPacket, [context.identifer]);
+            } else {
+                const validPackets = ["join", "leave", "message"];
+                if (!validPackets.includes(packet.type)) {
+                    console.log(`WARN: connection ID ${context.id} sent invalid packet type "${packet.type}"`);
+                    return;
+                }
+
+                // Handle it ourselves
+                this.handlePacket(packet, true);
+
+                // Distribute the packet except for the one that sent it
+                this.sendPacketToAll(packet, [context.identifier]);
+            }
+        });
+
+        connection.on("close", () => {
+            // Create the disconnect packet
+            const disconnectPacket = {type: "disconnect", identifier: context.identifier};
+
+            // Handle the disconnect packet ourselves (this will remove it)
+            this.handlePacket(disconnectPacket, true);
+
+            // Distribute the packet (no need for exclusions, the connection is already removed)
+            this.sendPacketToAll(disconnectPacket, []);
+        });
+    }
+
+    sendJoinPacket(username) {
+        const joinPacket = {type: "join", identifier: this.identifier, username};
+        this.sendPacketToAll(joinPacket, []);
+    }
+
+    sendLeavePacket(username) {
+        const leavePacket = {type: "leave", identifier: this.identifier, username};
+        this.sendPacketToAll(leavePacket, []);
+    }
+
+    sendMessagePacket(username, content) {
+        const messagePacket = {type: "message", identifier: this.identifier, username, content};
+        this.sendPacketToAll(messagePacket, []);
+    }
+
+    disconnect() {
+        this.connections.forEach((c) => c.connection.destroy());
+        this.connections.close();
+    }
+}
+
+// The client server derivation of ConnectionInstance.
+class ClientInstance extends ConnectionInstance {
+    constructor(name, color, prefix, ip, port, reconnectInterval) {
+        super(name, color, prefix);
+        this.ip = ip;
+        this.port = port;
+        this.client = new net.Socket();
+        this.acknowledgeReceived = false;
+        this.reconnectInterval = reconnectInterval;
+        this.reconnectTimeout = null;
+    }
+
+    sendPacket(packet) {
+        this.client.write(JSON.stringify(packet));
+    }
+
+    start(playerCount) {
+        this.client.connect(this.port, this.ip, () => {
+            // Send the handshake
+            const handshakePacket = {type: "handshake", name: this.name, color: this.color, prefix: this.prefix, playerCount};
+            this.sendPacket(handshakePacket);
+
+            this.client.on("data", (data) => {
+                const packet = JSON.parse(data);
+                if (!this.acknowledgeReceived && packet.type != "acknowledge") {
+                    console.log("FATAL: received non-ack packet before receiving ack");
+                    this.client.destroy();
+                    return;
+                }
+
+                if (packet.type == "acknowledge") {
+                    if (this.acknowledgeReceived) {
+                        console.log("FATAL: received ack packet more than once");
+                        this.client.destroy();
+                        return;
+                    }
+                    this.identifier = packet.identifier;
+                    this.hostIdentifier = packet.hostIdentifier;
+                    this.connections = packet.connections;
+                    this.acknowledgeReceived = true;
+                    console.log("INFO: received acknowledge from host server");
+                } else {
+                    // Handle packets normally
+                    this.handlePacket(packet);
+                }
+            });
+        });
+    }
+
+    sendJoinPacket(username) {
+        const joinPacket = {type: "join", identifier: this.identifier, username};
+        this.sendPacket(joinPacket);
+    }
+
+    sendLeavePacket(username) {
+        const leavePacket = {type: "leave", identifier: this.identifier, username};
+        this.sendPacket(leavePacket);
+    }
+
+    sendMessagePacket(username, content) {
+        const messagePacket = {type: "message", identifier: this.identifier, username, content};
+        this.sendPacket(messagePacket);
+    }
+
+    disconnect(attemptToReconnect) {
+        if (this.client != null) this.client.destroy();
+        if (attemptToReconnect) {
+            if (this.reconnectTimeout != null) return; // we are already waiting to reconnect
+            this.reconnectTimeout = setTimeout(() => {
+                this.reconnectTimeout = null;
+                if (this.client.readyState != "open") {
+                    this.client = new net.Socket();
+                    this.start(this.omegga.getPlayers().length);
+                }
+            }, this.reconnectInterval * 1000);
+        }
+    }
+}
 
 class CrossServerChat {
     constructor(omegga, config, store) {
@@ -7,183 +242,33 @@ class CrossServerChat {
         this.store = store;
     }
 
-    relayPacket(data, f) {
-        console.log(`Total connection count: ${this.connections.length}`);
-        this.connections.filter(c => c.connection != f).forEach(c => c.connection.write(data));
-    }
-
-    connect() {
-        if (this.hosting) {
-            this.connections = [];
-            this.server = net.createServer(c => this.handleConnection(c));
-            this.server.listen(this.config.port);
-        } else {
-            this.client = new net.Socket();
-            this.handshakeReceived = false;
-            this.serverName = null;
-            this.client.connect(this.config.port, this.config.ip, () => {
-                // send the handshake
-                this.client.write(JSON.stringify({"type": "handshake", "name": this.config.name, "playerCount": this.omegga.getPlayers().length, "color": this.config.color}));
-
-                this.client.on("data", (data) => {
-                    const packet = JSON.parse(data);
-
-                    if (packet.type == "message") {
-                        this.omegga.broadcast(`<color="${packet.color}">${packet.prefix} <b>${packet.name}</b></color>: ${packet.message}`);
-                    } else if (packet.type == "acknowledge") {
-                        if (this.handshakeReceived) {
-                            // we already received a handshake! ignore (todo: error handling)
-                            return;
-                        }
-
-                        this.handshakeReceived = true;
-                        this.serverName = packet.name;
-
-                        this.omegga.broadcast(`<color="${this.config.color}">Connected to <color="${packet.color}"><b>${packet.name}</b></color>, with ${packet.totalConnections - 1} other connections.</color>`);
-                    } else if (packet.type == "connection") {
-                        this.omegga.broadcast(`<color="${packet.color}"><b>${packet.name}</b></color> <color="${this.config.color}">has connected to chat with ${packet.playerCount} players online.</color>`);
-                    } else if (packet.type == "disconnection") {
-                        this.omegga.broadcast(`<color="${this.config.color}"><b>${packet.name}</b> has disconnected from chat.</color>`)
-                    } else if (packet.type == "join") {
-                        this.omegga.broadcast(`<color="${packet.color}">${packet.prefix} <b>${packet.name}</b> has joined <b>${packet.serverName}</b>.</color>`);
-                    } else if (packet.type == "leave") {
-                        this.omegga.broadcast(`<color="${packet.color}">${packet.prefix} <b>${packet.name}</b> has left <b>${packet.serverName}</b>.</color>`);
-                    }
-                });
-
-                this.client.on("close", () => {
-                    this.omegga.broadcast(`<color="${this.config.color}"><b>Connection closed with chat.</b> Reconnect using <code>!chat:reconnect</code>.</color>`);
-                    this.waitToReconnect();
-                });
-            });
-        }
-    }
-
-    waitToReconnect() {
-        if (this.reconnectTimeout != null && this.reconnectTimeout != undefined) return;
-        if (this.config["reconnect-interval"] <= 0) return;
-
-        this.reconnectTimeout = setTimeout(() => {
-            this.reconnectTimeout = null;
-            if (this.client.readyState != "open")
-                this.connect();
-        }, this.config["reconnect-interval"] * 1000);
-    }
-
-    disconnect(attemptToReconnect) {
-        if (this.hosting) {
-            if (this.server != null) {
-                this.connections.forEach(c => c.connection.destroy());
-                this.server.close();
-            }
-        } else {
-            if (this.client != null) this.client.destroy();
-            if (attemptToReconnect)
-                this.waitToReconnect();
-        }
-    }
-
-    handleConnection(c) {
-        console.log("New connection. Anticipating handshake...");
-
-        const connectionWrapper = {"connection": c, "receivedHandshake": false, "name": null};
-        this.connections.push(connectionWrapper);
-
-        c.on("data", data => {
-            const packet = JSON.parse(data);
-
-            if (packet.type == "handshake") {
-
-                connectionWrapper.receivedHandshake = true;
-                connectionWrapper.name = packet.name;
-                c.write(JSON.stringify({"type": "acknowledge", "name": this.config.name, "totalConnections": this.connections.length, "color": this.config.color}));
-
-                this.omegga.broadcast(`<color="${packet.color}"><b>${packet.name}</b></color> <color="${this.config.color}">has connected to chat with ${packet.playerCount} players online.</color>`);
-                this.relayPacket(JSON.stringify({"type": "connection", "name": packet.name, "playerCount": packet.playerCount, "color": packet.color}), c);
-
-            } else if (packet.type == "message") {
-
-                this.relayPacket(data, c);
-                this.omegga.broadcast(`<color="${packet.color}">${packet.prefix} <b>${packet.name}</b></color>: ${packet.message}`);
-
-            } else if (packet.type == "join") {
-
-                this.relayPacket(data, c);
-                this.omegga.broadcast(`<color="${packet.color}">${packet.prefix} <b>${packet.name}</b> has joined <b>${packet.serverName}</b>.</color>`);
-
-            } else if (packet.type == "leave") {
-
-                this.relayPacket(data, c);
-                this.omegga.broadcast(`<color="${packet.color}">${packet.prefix} <b>${packet.name}</b> has left <b>${packet.serverName}</b>.</color>`);
-
-            }
-        });
-
-        c.on("close", () => {
-            this.connections.splice(this.connections.indexOf(connectionWrapper), 1);
-
-            this.omegga.broadcast(`<color="${this.config.color}"><b>${connectionWrapper.name}</b> has disconnected from chat.</color>`);
-            this.relayPacket(JSON.stringify({"type": "disconnection", "name": connectionWrapper.name}), c);
-        });
-    }
-
     async init() {
-        this.hosting = this.config.hosting;
-
-        try {
-            this.connect();
-        } catch (e) {
-            console.log("Error connecting: " + e);
-            this.omegga.broadcast(`<color="${this.config.color}">Error connecting to chat. Reconnect using <code>!chat:reconnect</code></color>`);
+        this.connection = null;
+        if (this.hosting) {
+            const {name, color, prefix, port} = this.config;
+            this.connection = new HostServerInstance(name, color, prefix, port);
+        } else {
+            const {name, color, prefix, ip, port} = this.config;
+            this.connection = new ClientInstance(name, color, prefix, ip, port, this.config["reconnect-interval"]);
         }
 
-        this.omegga.on("chat", (name, message) => {
-            const packet = {"type": "message", "name": name, "message": message, "color": this.config.color, "prefix": this.config.prefix}
+        this.connection.start(this.omegga.getPlayers().length);
 
-            if (this.hosting) {
-                if (this.server == undefined || !this.server.listening) return;
-                this.connections.forEach(c => c.connection.write(JSON.stringify(packet)));
-            } else {
-                if (this.client == undefined || this.client.readyState != "open") return;
-                this.client.write(JSON.stringify(packet));
-            }
+        this.omegga.on("chat", (username, message) => {
+            this.connection.sendMessagePacket(username, parseLinks(sanitize(message)));
         });
 
-        this.omegga.on("join", (player) => {
-            const packet = {"type": "join", "name": player.name, "color": this.config.color, "prefix": this.config.prefix, "serverName": this.config.name};
-
-            if (this.hosting) {
-                if (this.server == undefined || !this.server.listening) return;
-                this.connections.forEach(c => c.connection.write(JSON.stringify(packet)));
-            } else {
-                if (this.client == undefined || this.client.readyState != "open") return;
-                this.client.write(JSON.stringify(packet));
-            }
+        this.omegga.on("join", (username) => {
+            this.connection.sendJoinPacket(username);
         });
 
-        this.omegga.on("leave", (player) => {
-            const packet = {"type": "leave", "name": player.name, "color": this.config.color, "prefix": this.config.prefix, "serverName": this.config.name};
-
-            if (this.hosting) {
-                if (this.server == undefined || !this.server.listening) return;
-                this.connections.forEach(c => c.connection.write(JSON.stringify(packet)));
-            } else {
-                if (this.client == undefined || this.client.readyState != "open") return;
-                this.client.write(JSON.stringify(packet));
-            }
-        });
-
-        this.omegga.on("chatcmd:chat:reconnect", (name) => {
-            if (!this.omegga.getPlayer(name).isHost() && !this.config["reconnect-authorized"].split(",").map((n) => n.trim().toLowerCase()).includes(name.toLowerCase())) return;
-
-            console.log("Attempting to reconnect...");
-            this.disconnect(false); // we are manually going to reconnect immediately after
-            this.connect();
+        this.omegga.on("leave", (username) => {
+            this.connection.sendLeavePacket(username);
         });
     }
 
     async stop() {
-        this.disconnect(false);
+        this.connection.disconnect(false);
     }
 }
 
